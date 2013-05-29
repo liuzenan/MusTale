3e4fd05451f0494ef7bdf30752d0fc45344a63e6
Liu Zenan <liuzenan711@gmail.com>
2013-05-30 00:11:53 +0800

    Initial Commit

---
+++ MusTale/MTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MTAppDelegate.h
+//  MusTale
+//
+//  Created by Zenan on 30/5/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MusTale/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MusTale
+//
+//  Created by Zenan on 30/5/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MTAppDelegate class]));
+    }
+}
